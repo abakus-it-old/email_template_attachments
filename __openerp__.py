344ae12977dc378fe272d8ac87ae1989e4ebf802
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:04:29 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Email template attachments",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ["mail"],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
